20b64debc24d595ce9a5b5a3f494ecbfaffda337
Emma Spencer <emma.n.spencer@gmail.com>
2013-10-22 20:54:00 -0400

    created gmaps4rails app, added devise to gemfile, fixed database.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gmaps4rails::Application.config.secret_key_base = '5d02e486310d5e8fd19db684c52228c842b6b68e17c59f4a7e22aa3dc8d5ccb2dded0b089bacdca112f8074c5635cf84e263c4c3e4c3a10fd0fdc38bc4ab2cd8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gmaps4rails::Application.config.session_store :cookie_store, key: '_gmaps4rails_session'
