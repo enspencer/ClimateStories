5d1b7cf2755c7bfdedef9156fe707184f69f2e60
Emma Spencer <emma.n.spencer@gmail.com>
2013-10-23 19:42:19 -0400

    Required username input at registration in devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) << :username << :location
+    devise_parameter_sanitizer.for(:sign_up) << :username << :location
   end
 
 end
