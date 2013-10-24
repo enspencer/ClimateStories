888d1cc984e5053b3bd58fd61b73d7676809b548
Emma Spencer <emma.n.spencer@gmail.com>
2013-10-23 20:22:12 -0400

    Created show page for user with username and stories

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,26 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@users = User.all
+	end
+
+	def show
+		@json = @user.stories.each do |story|
+			story.to_gmaps4rails do |story, marker|
+			marker.infowindow render_to_string(:partial => "/stories/infowindow", :locals => { :story => story})
+		end
+	end
+end
+
+
+	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+		params.require(:user).permit(:email, :username)
 	end
 end
