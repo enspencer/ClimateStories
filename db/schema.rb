06227c34813d5381fa73096ebd74f3513e72325e
Emma Spencer <emma.n.spencer@gmail.com>
2013-10-24 22:14:46 -0400

    Generated Comments controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131023123956) do
+ActiveRecord::Schema.define(version: 20131025021039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.string   "name"
+    t.text     "body"
+    t.integer  "story_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "stories", force: true do |t|
     t.string   "name"
