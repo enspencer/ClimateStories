460ba691dced166a8d0b86c3d89bf05322c771bb
Emma Spencer <emma.n.spencer@gmail.com>
2013-10-26 17:54:47 -0400

    works on heroku. testing twilio now

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,14 +1,25 @@
 class TwilioController < ApplicationController
-
-  def process_sms
-    @town = params[:FromCity].capitalize
-    @region = params[:FromState]
-    @country = params[:FromCountry]
-    @zipcode = params[:FromZip]
-    @body = params[:Body]
-    @sender = params[:From]
+	before_action :set_twilio
+	
+	  def process_sms
+	    @town = params[:FromCity].capitalize
+	    @region = params[:FromState]
+	    @country = params[:FromCountry]
+	    @zipcode = params[:FromZip]
+	   	@body = params[:Body]
+	    @name = params[:From]
 
     render 'process_sms.xml.erb', :content_type => 'text/xml'
   end
+
+  private
+
+	  def set_twilio
+	  	twilio = Twilio.find(params[:id])
+	  end
+
+	  def twilio_params
+	  	params.require(:twilio).permit(:FromCity, :FromState, :FromCountry, :FromZip, :Body, :From)
+	  end
 
 end
--- config/initializers/twilio_config.rb
+++ config/initializers/twilio_config.rb
@@ -1,6 +1,5 @@
-# user_sid= ENV['USER_SID']
-# auth_token= ENV['AUTH_TOKEN']
+ClimateStories::Application.config.account_sid = ENV['ACCOUNT_SID']
+ClimateStories::Application.config.auth_token = ENV['AUTH_TOKEN']
+ClimateStories::Application.config.phone_number = ENV['PHONE_NUMBER']
 
-# phone_number = ENV['PHONE_NUMBER']
-
-# TWILIO_CLIENT = Twilio::REST::Client.new ENV['USER_SID'], ENV['AUTH_TOKEN']
+TWILIO_CLIENT = Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
