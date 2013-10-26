b602a94102d593471aa5f2032b4cb99ce0502e70
Emma Spencer <emma.n.spencer@gmail.com>
2013-10-26 18:50:00 -0400

    stories working heroku but not adding text story

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,10 +1,7 @@
 class TwilioController < ApplicationController
   def process_sms
-    town = params["FromCity"].capitalize
-    zipcode = params["FromZip"]
-   	body = params["Body"]
     name = params["From"]
-    Story.create(zipcode: params["FromZip"], body: params["Body"])
+    Story.create(town: params["FromCity"].capitalize, zipcode: params["FromZip"], body: params["Body"])
     return '200'
   	#render 'process_sms.xml.erb', content_type: 'text/xml'
 	end
