7301b152032783e22d82ae0b893a4a83a812721a
Emma Spencer <emma.n.spencer@gmail.com>
2013-10-27 20:43:08 -0400

    still issues w towns

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,6 +1,6 @@
 class TwilioController < ApplicationController
   def process_sms
-    name = params["From"]
+    name = params["FromCity"]
     Story.create(town: params["FromCity"].capitalize, zipcode: params["FromZip"], body: params["Body"])
     return '200'
   	#render 'process_sms.xml.erb', content_type: 'text/xml'
